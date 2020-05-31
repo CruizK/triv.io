766810d33cf005f65e10d985444d0265f0fb960d
CruizK <Cruiz.Kusman@gmail.com>
2020-05-31 06:29:49 -0500

    Fixed time desync with css animation bug using 5Head manouvers

--- app/components/Timer/Timer.js
+++ app/components/Timer/Timer.js
@@ -9,9 +9,8 @@ const Timer = ({time, timerPlayState}) => {
     <div className={styles.timerText}>{time}</div>
     <svg className={styles.svg}>
       <circle r="27" cx="30" cy="30" className={styles.timerBackCircle}></circle>
-      {timerPlayState == 'restart' ? null :
-        <circle r="27" cx="30" cy="30" className={styles.timerCircle} style={{animationPlayState:timerPlayState}}></circle>
-      }
+      <circle r="27" cx="30" cy="30" className={styles.timerCircle} 
+        style={{animationPlayState:timerPlayState, animationName: timerPlayState == 'restart' ? 'none' : styles.countdown}}></circle>
     </svg>
   </div>
   )
